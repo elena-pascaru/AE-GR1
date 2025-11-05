10387874e2a62ddaca38ea386dab038160b552e3
elena-pascaru <pascaruelena20@stud.ase.ro>
2025-11-05 19:35:10 +0200

    tested variables

---
+++ test.js
@@ -0,0 +1,45 @@
+// var x = 5;
+// let y = 6;
+// const z = 7;
+
+// console.log(x, y, z);
+
+// x = 6;
+// console.log(x);
+
+// y = 7;
+// console.log(y);
+
+// z = 8;
+//console.log(z);
+
+// function testVariables(param) {
+
+//     if(param ==- true) {
+//         var x = 5;
+//         let y = 6;
+//         const z = 7;
+//     }
+
+//     console.log(x);
+//     console.log(y);
+//     console.log(z);
+// }
+
+
+// testVariables(true);
+
+const testVariables2  = (param) => {
+    console.log(x);
+    if(param ==- true) {
+        var x = 5;
+        let y = 6;
+        const z = 7;
+    }
+
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+
+testVariables2(true);
